adcabc5c9da7141800a0a9c193cef9274b4a97b5
gwanhwikim <rhksgnl11@naver.com>
2022-06-18 16:15:37 +0900

    change db

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 let serviceAccount = require("./firebase-admin.json");
 
 admin.initializeApp({
-    credential : admin.credential.cert(serviceAccount),
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234'
-})
+  host: "localhost",
+  user: "root",
+  password: "dk1234",
+});
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('connected');
-    
-})
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("connected");
+});
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/',  (req,res) => {
-    res.send('Hello World')
-})
+app.get("/push", async (req, res) => {
+  console.log(req.body);
+  let message = {
+    notification: {
+      title: req.query.title,
+      body: req.query.content,
+    },
+    token: req.query.token,
+  };
 
-app.get('/push', async (req, res) => {
-    console.log(req.body);
-    let message = {
-        notification: {
-            title:req.query.title,
-            body: req.query.content,
-        },
-        token: req.query.token,
-    }
-
-    admin.messaging().send(message).then(function (res) {
-        console.log('success sent message : ', res)
-    }).catch(function (err) {
-        console.log('err : ', err)
+  admin
+    .messaging()
+    .send(message)
+    .then(function (res) {
+      console.log("success sent message : ", res);
     })
-})
+    .catch(function (err) {
+      console.log("err : ", err);
+    });
+});
 
 app.listen(3000);
